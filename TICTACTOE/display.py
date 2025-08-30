b5b1fd03282f4572898171e64e73d7bd05c36872
Jay Kumar <jayk13679@gmail.com>
2025-08-30 23:39:41 +0530

    Update display.py

--- TICTACTOE/display.py
+++ TICTACTOE/display.py
@@ -1,1 +1,20 @@
-
+def design() :
+              h = "|"
+              g = "---------"
+              
+              l = [1,2,3,4,5,6,7,8,9]      #position list
+              print("STARTING THE GAME")
+              print("T | I | C")
+              print(g)
+              print("T | A | C")
+              print(g)
+              print("T | O | E")
+              print("Welcome Players")
+              #printing for position
+              print(l[0],h,l[1],h,l[2])
+              print(g)
+              print(l[3],h,l[4],h,l[5])
+              print(g)
+              print(l[6],h,l[7],h,l[8])
+              print("You have to play according to these positions")
+design()
